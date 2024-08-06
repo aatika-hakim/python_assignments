9e665a861f397576c1705c40cd120952e662d361
Aatika hakim <aatikahakim90@gmail.com>
2024-08-06 21:50:20 +0500

    prompts from users

---
+++ Ass_2.py
@@ -0,0 +1,23 @@
+# 1. **Add two numbers**
+
+#    Write a Python program that takes two integer inputs from the user and calculates their sum. The program should perform the following tasks:
+
+# 1: Prompt the user to enter the first number
+first_number_input = input("Enter the first number: ")
+
+# 2: Read the input and convert it to an integer
+first_number = int(first_number_input)
+
+# 3: Prompt the user to enter the second number
+second_number_input = input("Enter the second number: ")
+
+# 4: Read the input and convert it to an integer
+second_number = int(second_number_input)
+
+# 5: Calculate the sum of the two numbers
+sum_of_numbers = first_number + second_number
+print("The sum of the two numbers is:", sum_of_numbers)
+
+
+
+
